1fb2f6a2177ad5db33abd726578f71341e3d89b6
denghaoluan <1102651442@qq.com>
2025-08-11 23:00:01 +0800

    feat(config): 添加 YAML 配置文件解析功能并更新用户代理列表
    - 在 config_loader.py 中添加 read_yaml_file 方法，用于解析 YAML 配置文件
    - 更新 user_agent.py，将单个用户代理字符串改为用户代理列表

--- config/config_loader.py
+++ config/config_loader.py
@@ -11,6 +11,8 @@
 
 import os
 
+import yaml
+
 
 class ConfigLoader:
     def __init__(self, file_path):
@@ -22,3 +24,25 @@ class ConfigLoader:
         if os.path.exists(self.file_path):
             return True
         return False
+
+    def read_yaml_file(self):
+        """
+        读取YAML文件并返回字典
+        :returns: dict: 解析后的字典数据
+        """
+        if self.file_path:
+            try:
+                with open(self.file_path, 'r', encoding='utf-8') as file:
+                    data = yaml.safe_load(file)
+                    return data if data is not None else {}
+            except FileNotFoundError:
+                print(f"文件 {self.file_path} 不存在")
+                return {}
+            except yaml.YAMLError as e:
+                print(f"YAML格式错误: {e}")
+                return {}
+            except Exception as e:
+                print(f"读取文件时发生错误: {e}")
+                return {}
+        else:
+            print(f'文件不存在')
