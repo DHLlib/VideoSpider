4fac7b30bece5f14a6f13b48c9af8f20aa56d8ce
denghaoluan <denghaoluan@thefitville.com>
2025-08-12 17:55:54 +0800

    feat(control): 添加基础控制器并实现分类文件更新功能
    - 新增 Base_control.py 文件，实现分类文件更新功能
    - 使用 Class_fetcher 模块进行分类文件更新- 打印更新完成提示信息

---
+++ control/Base_control.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @author: DENGHAOLUAN
+# @file: Base_control.py
+# @time: 2025/8/12 17:09
+# DO EVERYTHING WHET YOU WANT
+
+# 基础控制器
+
+from core.Class_fetcher import ClassFetcher
+
+ClassFetcher() # 更新分类文件Class.json
+print('更新分类完成...')
+
