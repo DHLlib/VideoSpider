644483b01b47c782ec5678f3a1cfa8f7bfc6d5d4
denghaoluan <denghaoluan@thefitville.com>
2025-08-13 14:46:54 +0800

    build(config|core|spiders): 创建初始化文件
    - 在 config、core 和 spiders 目录下创建 __init__.py 文件

---
+++ config/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @author: DENGHAOLUAN
+# @file: __init__.py
+# @time: 2025/8/13 14:45
+# DO EVERYTHING WHET YOU WANT
---
+++ spiders/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @author: DENGHAOLUAN
+# @file: __init__.py
+# @time: 2025/8/13 14:46
+# DO EVERYTHING WHET YOU WANT
