6169cb437811e2d65afff6fd924bec1cf9dc185f
denghaoluan <denghaoluan@thefitville.com>
2025-08-14 12:17:01 +0800

    feat(models): 添加数据传输对象模型
    - 新建 models 目录及 __init__.py 文件
    - 创建 DTOs.py 文件，定义视频详情和搜索结果的数据传输对象- 包含 VideoEpisodeDTO、VideoDetailDTO、SearchEpisodeDTO 和 VideoSearchDTO

---
+++ models/DTOs.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @author: DENGHAOLUAN
+# @file: DTOs.py
+# @time: 2025/8/14 10:29
+# DO EVERYTHING WHET YOU WANT
+
+# 数据传输对象模型
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class VideoEpisodeDTO: # 视频集
+    episode_name: str # 集名
+    episode_url: str # 集的URL
+
+
+@dataclass
+class VideoDetailDTO:
+    name: str
+    url_list: List[list[VideoEpisodeDTO]]
+    status: str
+    total: str
+    remarks: str
+
+
+@dataclass
+class SearchEpisodeDTO: # 搜索集
+    name: str
+    vod_id: str
+
+
+@dataclass
+class VideoSearchDTO:
+    search_list: List[SearchEpisodeDTO]
