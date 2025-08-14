27c6fbff169f3ebe595984502be9d818fddf13d5
denghaoluan <1102651442@qq.com>
2025-08-14 21:47:27 +0800

    refactor(config): 更新 video_source.yaml 文件路径获取方法
    - 将文件路径获取方法从使用 os.getcwd() 修改为使用 __file__属性
    - 新方法能够更准确地定位到项目的 config 目录，提高代码的可移植性和鲁棒性

--- config/setting.py
+++ config/setting.py
@@ -9,7 +9,7 @@ import os
 from config import user_agent
 from config.config_loader import ConfigLoader
 
-_file_path = os.path.join(os.path.dirname(os.getcwd()), 'config', 'video_source.yaml')
+_file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config', 'video_source.yaml')
 YML_CONFIG = dict(ConfigLoader(_file_path).read_yaml_file())  # 读取资源网地址
 USER_AGENT = user_agent.USER_AGENTS[0]
 USER_AGENTS = user_agent.USER_AGENTS
