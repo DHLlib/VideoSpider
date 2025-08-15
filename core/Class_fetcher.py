2edc56c6545ef5601b551bdd979ba05cf537c75f
denghaoluan <denghaoluan@thefitville.com>
2025-08-15 10:34:28 +0800

    refactor(core): 重构 ClassFetcher 类
    - 继承 BaseFetcher 类，简化代码结构
    -优化类初始化和请求逻辑，提高代码可读性
    - 添加成功更新类别列表的打印信息
    - 移除未使用的导入和注释，清理代码

--- core/Class_fetcher.py
+++ core/Class_fetcher.py
@@ -7,33 +7,32 @@
 import json
 import os
 
-import requests
-
-from config.setting import YML_CONFIG, USER_AGENT
-
-_yml_config = YML_CONFIG.get('ffzy')
+from core.Base_fetcher import BaseFetcher
 
 
 # 获取类别
-class ClassFetcher:
+class ClassFetcher(BaseFetcher):
     def __init__(self):
-        self._base_url = _yml_config.get('base_url')
-        self._headers = {
-            'user-agent': USER_AGENT
-        }
+        super().__init__(type_='index', resource_name='ffzy')
         self._class_list = None
-        self._get_class_list()
+        self._request()
 
-    # 获取类别列表
-    def _get_class_list(self):
-        result = requests.get(url=self._base_url, headers=self._headers).json()
-        class_list = result.get('class')
-        self._class_list = class_list # 赋值给类变量
-        if class_list:
-            self._clean_class_json_file()
-            print('正在更新类别列表...')
-            with open('../config/Class.json', 'w',encoding='utf-8') as w:
-                json.dump(class_list, w, ensure_ascii=False, indent=4)
+    # 请求接口
+    def _request(self):
+        result = super()._request()
+        self._response_handler(result)
+
+    # 处理返回数据
+    def _response_handler(self, result):
+        if result:
+            class_list = result.get('class')
+            self._class_list = class_list  # 赋值给类变量
+            if class_list:
+                self._clean_class_json_file()
+                print('正在更新类别列表...')
+                with open('../config/Class.json', 'w', encoding='utf-8') as w:
+                    json.dump(class_list, w, ensure_ascii=False, indent=4)
+                print('更新类别列表成功...')
 
     # 清空文件内容
     @staticmethod
@@ -46,7 +45,6 @@ class ClassFetcher:
     # 获取类别列表
     def get_class_list(self):
         return self._class_list
-
 
 
 if __name__ == '__main__':
