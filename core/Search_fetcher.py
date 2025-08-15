7c77615e6ed91e8d121a2c570c77f0d453c6e2e6
denghaoluan <denghaoluan@thefitville.com>
2025-08-15 16:04:17 +0800

    refactor(core): 重构 Search_fetcher.py
    - 继承 BaseFetcher 类，简化代码结构
    - 优化搜索逻辑，提高代码可读性
    - 移除冗余方法，精简代码
    - 更新参数处理方式，提高灵活性

--- core/Search_fetcher.py
+++ core/Search_fetcher.py
@@ -4,54 +4,37 @@
 # @file: Search_fetcher.py
 # @time: 2025/8/12 14:04
 # DO EVERYTHING WHET YOU WANT
-import json
 
-import requests
-
-from config.setting import YML_CONFIG, USER_AGENT
+from core.Base_fetcher import BaseFetcher
 from models.DTOs import SearchEpisodeDTO, VideoSearchDTO
-
-_yml_config = YML_CONFIG.get('ffzy')
 
 
 # 搜索视频提取器
-class SearcherFetcher:
+class SearcherFetcher(BaseFetcher):
     def __init__(self, video_name, page=1, page_size=20):
+        fetcher_type = 'search'
+        super().__init__(type_=fetcher_type, resource_name='ffzy')
+        self._local_params = self._params.copy()
         self.video_name = video_name
-        self._base_url = _yml_config.get('base_url')
-        self._headers = {
-            'user-agent': USER_AGENT
-        }
-        self._search_path = _yml_config.get('search')
-        self._page = _yml_config.get('page')
-        self._page_size = _yml_config.get('page_size')
-        self._search_url = None
         self._search_result_list = []
-        self._search_video()
+        self._get_search_all_result()
 
     # 魔术方法
     # 当打印SearcherFetcher对象时，会直接显示video_name的值
     def __str__(self):
         return self.video_name
 
-    # 构建searchurl
-    def _build_search_url(self, page=1, page_size=100):
-        return (f"{self._base_url}"
-                f"{self._search_path}"
-                f"{self.video_name}&"
-                f"{self._page}{page}&"
-                f"{self._page_size}{page_size}")
-
     # 搜索视频
-    def _search_video(self):
+    def _get_search_all_result(self):
+        self._local_params['wd'] = self.video_name
         page_ = 1
         while True:
+            self._local_params['pg'] = page_
             print(f"正在搜索第{page_}页...")
-            self._search_url = self._build_search_url(page=page_)
-            result = requests.get(url=self._search_url, headers=self._headers).text
-            search_result = json.loads(result)
-            self._handle_search_result(search_result)
-            page_count = search_result.get('pagecount')
+            search_all_result = super()._request(params=self._local_params)
+            print(search_all_result)
+            self._handle_search_result(search_all_result)
+            page_count = search_all_result.get('pagecount')
             if page_count <= page_:
                 break
             else:
@@ -59,30 +42,22 @@ class SearcherFetcher:
 
     # 处理查询结果
     def _handle_search_result(self, search_result):
-        for i in search_result['list']:
+        for search_index in search_result['list']:
             search_data = SearchEpisodeDTO(
-            name=i.get('vod_name'),
-            vod_id = i.get('vod_id')
+                name=search_index.get('vod_name'),
+                vod_id=search_index.get('vod_id')
             )
             self._search_result_list.append(search_data)  # 列表追加
-
-
         print(f'数据合并完成')
 
     # 获取查询结果
     def get_search_result_list(self):
         return VideoSearchDTO(search_list=self._search_result_list)
 
-    # 获取url
-    def get_search_url(self):
-        return self._search_url
-
 
 if __name__ == '__main__':
-    s = SearcherFetcher('星期三')
+    s = SearcherFetcher('跑')
     result_list = s.get_search_result_list()
-    url = s.get_search_url()
     for i in result_list.search_list:
         print(i.name)
         print(i.vod_id)
-    print( url)
